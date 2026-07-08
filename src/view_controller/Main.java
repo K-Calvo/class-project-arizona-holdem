5f5dc5bed7fbc881c96682905fee39df12e46bad
K-Calvo <134893433+K-Calvo@users.noreply.github.com>
2026-07-07 19:29:04 -0700

    Create Main.java

---
+++ src/view_controller/Main.java
@@ -0,0 +1,14 @@
+// Author: Kianny Calvo
+
+package view_controller;
+
+import model.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game theGame = new Game();
+		theGame.start();
+	}
+
+}
